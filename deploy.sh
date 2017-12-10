d020200a1ec23f0a2bdc6c44816f439f3919df78
NEMS Linux <nems@baldnerd.com>
2017-12-09 19:14:11 -0500

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-apt -y install software-properties-common
-apt add-apt-repository ppa:webupd8team/java
 apt update
-apt -y install oracle-java8-installer unzip
+apt -y install software-properties-common dirmngr unzip
+add-apt-repository ppa:webupd8team/java
+apt update
+apt -y install oracle-java8-installer
 wget https://github.com/ant-media/Ant-Media-Server/releases/download/ams-v1.1.1/ant-media-server-1.1.1.zip -O /tmp/ams.zip
 cd /tmp
 unzip ams.zip
