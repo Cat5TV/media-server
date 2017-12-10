079fd605ffb94006cac4d8446958724a3eee55a9
NEMS Linux <nems@baldnerd.com>
2017-12-09 20:21:30 -0500

    Deploy media server

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,17 @@ apt -y install oracle-java8-installer jsvc
 wget https://github.com/ant-media/Ant-Media-Server/releases/download/ams-v1.1.1/ant-media-server-1.1.1.zip -O /tmp/ams.zip
 cd /tmp
 unzip ams.zip
+
+mv ant-media-server /usr/local/antmedia
+cp /usr/local/antmedia/antmedia /etc/init.d/
+update-rc.d antmedia defaults
+update-rc.d antmedia enable
+
+useradd -d /usr/local/antmedia/ -s /bin/false -r antmedia
+chown -R antmedia:antmedia /usr/local/antmedia/
+
+service antmedia start
+
+service antmedia status
+
+echo "Running on Port 5080"
